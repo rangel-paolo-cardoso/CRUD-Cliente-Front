352fbf8fd4105f94c263a5360a9bc09e94605fb3
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-26 20:34:09 -0300

    Página de Login Completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent,
+    PageNotFoundComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginPageComponent },
+      { path: 'MainScreen', component: MainScreenComponent },
+      { path: '**', component: PageNotFoundComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
