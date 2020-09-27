87121d5564aee9519de2232b15874203bd4abd92
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-27 14:46:09 -0300

    Roteamento resolvido por enquanto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +17,23 @@ import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes
     PageNotFoundComponent,
     LoginPageComponent,
     HomePageComponent,
-    ConsultaClientesComponent
+    ConsultaClientesComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: LoginPageComponent },
-      { path: 'MainScreen', component: MainScreenComponent },
-      { path: 'ConsultaClientes', component: ConsultaClientesComponent },
+      {
+        path: 'MainScreen',
+        component: MainScreenComponent,
+        children: [
+          { path: '', component: HomePageComponent },
+          { path: 'ConsultaClientes', component: ConsultaClientesComponent },
+          { path: 'CadastroCliente', component: CadastroClienteComponent }
+        ]
+      },
+      // { path: 'ConsultaClientes', component: ConsultaClientesComponent },
       { path: '**', component: PageNotFoundComponent }
     ]),
   ],
