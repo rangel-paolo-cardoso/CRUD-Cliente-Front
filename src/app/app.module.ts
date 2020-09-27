c621c54ac089bce686f0a13520bf9246ccd8abfa
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-26 21:25:51 -0300

    Criadas rotas para cada página.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainScreenComponent,
     PageNotFoundComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HomePageComponent,
+    ConsultaClientesComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: LoginPageComponent },
       { path: 'MainScreen', component: MainScreenComponent },
+      { path: 'ConsultaClientes', component: ConsultaClientesComponent },
       { path: '**', component: PageNotFoundComponent }
     ]),
   ],
